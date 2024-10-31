ca1d1376a87a0b8e68fe6cecd8d197554083c4f3
Judel Fintch <judfintch@gmail.com>
2024-10-31 16:31:06 +0200

    feat: change slide design

--- app/Livewire/Guest/Contact/Contact.php
+++ app/Livewire/Guest/Contact/Contact.php
@@ -3,9 +3,11 @@
 namespace App\Livewire\Guest\Contact;
 
 use Livewire\Component;
+use Livewire\Attributes\Layout;
 
 class Contact extends Component
 {
+    #[Layout('layouts.guest')]
     public function render()
     {
         return view('livewire.guest.contact.contact');
