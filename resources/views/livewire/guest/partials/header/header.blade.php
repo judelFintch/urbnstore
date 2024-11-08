1f365e305c8930fae7329b744911c3b358d33193
Judel Fintch <judfintch@gmail.com>
2024-11-08 22:02:01 +0200

    feat :change menu dispo

--- resources/views/livewire/guest/partials/header/header.blade.php
+++ resources/views/livewire/guest/partials/header/header.blade.php
@@ -1,142 +1,102 @@
 <div>
     <header class="header-v2">
-        <!-- Header desktop -->
-        <div class="container-menu-desktop trans-03">
+        <!-- Header pour bureau -->
+        <div class="container-menu-desktop">
             <div class="wrap-menu-desktop">
                 <nav class="limiter-menu-desktop p-l-45">
-                    <!-- Logo desktop -->
+                    <!-- Logo -->
                     <a href="#" class="logo">
                         <img src="{{ asset('images/icons/lg.png') }}" alt="IMG-LOGO">
                     </a>
-                    <!-- Menu desktop -->
+                    
+                    <!-- Menu pour bureau -->
                     <div class="menu-desktop">
                         <ul class="main-menu">
-                            <li class="active-menu">
-                                <a href="{{ route('home.index') }}">Home</a>
-                            </li>
-                            <li>
-                                <a
-                                    href="{{ route('home.shop', ['id' => $defaultCategoryArticles, 'slug' => $defaultUrl]) }}">Shop</a>
-                            </li>
+                            <li class="active-menu"><a href="{{ route('home.index') }}">Home</a></li>
+                            <li><a href="{{ route('home.shop', ['id' => $defaultCategoryArticles, 'slug' => $defaultUrl]) }}">Shop</a></li>
                             @foreach ($categoryArticles as $categoryArticle)
-                                <li>
-                                    <a
-                                        href="{{ route('home.shop', ['id' => $categoryArticle->id, 'slug' => $categoryArticle->slug]) }}">
-                                        {{ $categoryArticle->name }}
-                                    </a>
-                                </li>
+                                <li><a href="{{ route('home.shop', ['id' => $categoryArticle->id, 'slug' => $categoryArticle->slug]) }}">{{ $categoryArticle->name }}</a></li>
                             @endforeach
-                            <li>
-                                <a href="{{ route('home.about') }}">About</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('home.contact') }}">Contact</a>
-                            </li>
+                            <li><a href="{{ route('home.about') }}">About</a></li>
+                            <li><a href="{{ route('home.contact') }}">Contact</a></li>
                         </ul>
                     </div>
-                    <!-- Icon header -->
+    
+                    <!-- Icônes pour bureau -->
                     <div class="wrap-icon-header flex-w flex-r-m h-full">
-                        <div class="flex-c-m h-full p-r-24">
-                            <div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 js-show-modal-search">
-                                <i class="zmdi zmdi-search"></i>
-                            </div>
+                        <!-- Icône de recherche -->
+                        <div class="icon-header-item cl2 p-lr-15 js-show-modal-search">
+                            <i class="zmdi zmdi-search"></i>
                         </div>
-
-                        <div class="flex-c-m h-full p-l-18 p-r-25 bor5">
-                            <div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart"
-                                data-notify="2">
-                                <i class="zmdi zmdi-shopping-cart"></i>
-                            </div>
+                        <!-- Icône de panier -->
+                        <div class="icon-header-item cl2 p-lr-15 js-show-cart" data-notify="2">
+                            <i class="zmdi zmdi-shopping-cart"></i>
                         </div>
-
-                        <div class="flex-c-m h-full p-lr-19">
-                            <div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 js-show-sidebar">
-                                <i class="zmdi zmdi-menu"></i>
-                            </div>
+                        <!-- Icône de menu latéral -->
+                        <div class="icon-header-item cl2 p-lr-15 js-show-sidebar">
+                            <i class="zmdi zmdi-menu"></i>
+                        </div>
+                        <!-- Sélecteur de langue -->
+                        <div class="icon-header-item cl2 p-lr-15">
+                            <select id="language-select" class="form-control" onchange="changeLanguage(this)">
+                                <option value="en">English</option>
+                                <option value="fr">Français</option>
+                                <option value="es">Español</option>
+                            </select>
+                        </div>
+                        <!-- Sélecteur de devise -->
+                        <div class="icon-header-item cl2 p-lr-15">
+                            <select id="currency-select" class="form-control" onchange="changeCurrency(this)">
+                                <option value="USD">USD</option>
+                                <option value="EUR">EUR</option>
+                                <option value="GBP">GBP</option>
+                            </select>
+                        </div>
+                        <!-- Icône de connexion -->
+                        <div class="icon-header-item cl2 p-lr-15">
+                            <a href="{{ route('login') }}">
+                                <i class="fas fa-user"></i>
+                            </a>
                         </div>
                     </div>
                 </nav>
             </div>
         </div>
-
-        <!-- Header Mobile -->
+    
+        <!-- Header pour mobile -->
         <div class="wrap-header-mobile">
-            <!-- Logo moblie -->
             <div class="logo-mobile">
                 <img src="{{ asset('images/icons/lg.png') }}" alt="IMG-LOGO">
             </div>
-            <!-- Icon header -->
             <div class="wrap-icon-header flex-w flex-r-m h-full m-r-15">
-                <div class="flex-c-m h-full p-r-10">
-                    <div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 js-show-modal-search">
-                        <i class="zmdi zmdi-search"></i>
-                    </div>
+                <div class="icon-header-item cl2 p-lr-15 js-show-modal-search">
+                    <i class="zmdi zmdi-search"></i>
                 </div>
-
-                <div class="flex-c-m h-full p-lr-10 bor5">
-                    <div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart"
-                        data-notify="2">
-                        <i class="zmdi zmdi-shopping-cart"></i>
-                    </div>
+                <div class="icon-header-item cl2 p-lr-15 js-show-cart" data-notify="2">
+                    <i class="zmdi zmdi-shopping-cart"></i>
                 </div>
             </div>
-
-            <!-- Button show menu -->
-            <div class="btn-show-menu-mobile hamburger hamburger--squeeze">
+            <!-- Icône du menu hamburger pour mobile uniquement -->
+            <div class="btn-show-menu-mobile hamburger hamburger--squeeze" id="hamburger-menu">
                 <span class="hamburger-box">
                     <span class="hamburger-inner"></span>
                 </span>
             </div>
         </div>
-
-        <!-- Menu Mobile -->
-        <div class="menu-mobile">
+    
+        <!-- Menu mobile -->
+        <div class="menu-mobile" id="menu-mobile">
             <ul class="main-menu-m">
-                <li>
-                    <a href="index.html">Home</a>
-                    <ul class="sub-menu-m">
-                        <li><a href="index.html">Homepage 1</a></li>
-
-                    </ul>
-                    <span class="arrow-main-menu-m">
-                        <i class="fa fa-angle-right" aria-hidden="true"></i>
-                    </span>
-                </li>
-
-                <li>
-                    <a
-                        href="{{ route('home.shop', ['id' => $defaultCategoryArticles, 'slug' => $defaultUrl]) }}">Shop</a>
-                </li>
+                <li><a href="{{ route('home.index') }}">Home</a></li>
+                <li><a href="{{ route('home.shop', ['id' => $defaultCategoryArticles, 'slug' => $defaultUrl]) }}">Shop</a></li>
                 @foreach ($categoryArticles as $categoryArticle)
-                    <li>
-                        <a
-                            href="{{ route('home.shop', ['id' => $categoryArticle->id, 'slug' => $categoryArticle->slug]) }}">
-                            {{ $categoryArticle->name }}
-                        </a>
-                    </li>
+                    <li><a href="{{ route('home.shop', ['id' => $categoryArticle->id, 'slug' => $categoryArticle->slug]) }}">{{ $categoryArticle->name }}</a></li>
                 @endforeach
-                <li>
-                    <a href="{{ route('home.about') }}">About</a>
-                </li>
-                <li>
-                    <a href="{{ route('home.contact') }}">Contact</a>
-                </li>
+                <li><a href="{{ route('home.about') }}">About</a></li>
+                <li><a href="{{ route('home.contact') }}">Contact</a></li>
             </ul>
         </div>
-        <!-- Modal Search -->
-        <div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
-            <div class="container-search-header">
-                <button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
-                    <img src="images/icons/icon-close2.png" alt="CLOSE">
-                </button>
-
-                <form class="wrap-search-header flex-w p-l-15">
-                    <button class="flex-c-m trans-04">
-                        <i class="zmdi zmdi-search"></i>
-                    </button>
-                    <input class="plh3" type="text" name="search" placeholder="Search...">
-                </form>
-            </div>
-        </div>
     </header>
+
+
 </div>
