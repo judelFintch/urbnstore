9982a315304733746006a42c9104c9a724779426
Judel Fintch <judfintch@gmail.com>
2024-11-21 16:04:24 +0200

    feat :adding about texte

--- resources/views/livewire/guest/about/about.blade.php
+++ resources/views/livewire/guest/about/about.blade.php
@@ -1,77 +1,97 @@
 <div>
-       
-	<!-- Title page -->
-	<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/bg-01.jpg');">
-		<h2 class="ltext-105 cl0 txt-center">
-			About
-		</h2>
-	</section>	
+
+    <!-- Title page -->
+    <section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/bg-01.jpg');">
+        <h2 class="ltext-105 cl0 txt-center">
+            About
+        </h2>
+    </section>
 
 
-	<!-- Content page -->
-	<section class="bg0 p-t-75 p-b-120">
-		<div class="container">
-			<div class="row p-b-148">
-				<div class="col-md-7 col-lg-8">
-					<div class="p-t-7 p-r-85 p-r-15-lg p-r-0-md">
-						<h3 class="mtext-111 cl2 p-b-16">
-							Our Story
-						</h3>
+    <!-- Content page -->
+    <section class="bg0 p-t-75 p-b-120">
+        <div class="container">
+            <div class="row p-b-148">
+                <div class="col-md-7 col-lg-8">
+                    <div class="p-t-7 p-r-85 p-r-15-lg p-r-0-md">
+                        <h3 class="mtext-111 cl2 p-b-16">
+                            Notre histoire
+                        </h3>
 
-						<p class="stext-113 cl6 p-b-26">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat consequat enim, non auctor massa ultrices non. Morbi sed odio massa. Quisque at vehicula tellus, sed tincidunt augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas varius egestas diam, eu sodales metus scelerisque congue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas gravida justo eu arcu egestas convallis. Nullam eu erat bibendum, tempus ipsum eget, dictum enim. Donec non neque ut enim dapibus tincidunt vitae nec augue. Suspendisse potenti. Proin ut est diam. Donec condimentum euismod tortor, eget facilisis diam faucibus et. Morbi a tempor elit.
-						</p>
+                        <p class="stext-113 cl6 p-b-26">
+                            C'est en observant la scène mode à Lubumbashi que <b>Moise A. NGONGO</b> a perçu une opportunité
+                            unique : créer une marque qui non seulement suit les tendances internationales, mais célèbre
+                            aussi l’héritage local. Cette dualité entre global et local est au cœur de la philosophie
+                            d’Urban Store. Dès ses débuts, Urban Store a misé sur la qualité, l’authenticité, et une
+                            expérience client unique, que ce soit en ligne ou en magasin. Chaque collection et chaque
+                            article sont sélectionnés avec soin pour permettre à ses clients de s’exprimer librement à
+                            travers leur style. Urban Store est bien plus qu’un commerce ; c’est une communauté qui
+                            valorise la créativité et célèbre l’individualité
+                        </p>
 
-						<p class="stext-113 cl6 p-b-26">
-							Donec gravida lorem elit, quis condimentum ex semper sit amet. Fusce eget ligula magna. Aliquam aliquam imperdiet sodales. Ut fringilla turpis in vehicula vehicula. Pellentesque congue ac orci ut gravida. Aliquam erat volutpat. Donec iaculis lectus a arcu facilisis, eu sodales lectus sagittis. Etiam pellentesque, magna vel dictum rutrum, neque justo eleifend elit, vel tincidunt erat arcu ut sem. Sed rutrum, turpis ut commodo efficitur, quam velit convallis ipsum, et maximus enim ligula ac ligula. 
-						</p>
 
-						<p class="stext-113 cl6 p-b-26">
-							Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879
-						</p>
-					</div>
-				</div>
+						<h3 class="mtext-111 cl2 p-b-16"> Notre vision :</h3>
+                        <p class="stext-113 cl6 p-b-26">
+                            Urban Store est né avec une mission claire : réinventer le style urbain et contemporain en
+                            proposant des produits qui reflètent les besoins et les aspirations de ses clients. Fondée
+                            par <b>Moise A. NGONGO</b>, l’entreprise se positionne comme un pont entre les tendances mondiales
+                            et les influences locales, faisant de la mode un puissant vecteur d'expression personnelle
+                            et culturelle.
 
-				<div class="col-11 col-md-5 col-lg-4 m-lr-auto">
-					<div class="how-bor1 ">
-						<div class="hov-img0">
-							<img src="images/about-01.jpg" alt="IMG">
-						</div>
-					</div>
-				</div>
-			</div>
-			
-			<div class="row">
-				<div class="order-md-2 col-md-7 col-lg-8 p-b-30">
-					<div class="p-t-7 p-l-85 p-l-15-lg p-l-0-md">
-						<h3 class="mtext-111 cl2 p-b-16">
-							Our Mission
-						</h3>
+                        </p>
+                    </div>
+                </div>
 
-						<p class="stext-113 cl6 p-b-26">
-							Mauris non lacinia magna. Sed nec lobortis dolor. Vestibulum rhoncus dignissim risus, sed consectetur erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam maximus mauris sit amet odio convallis, in pharetra magna gravida. Praesent sed nunc fermentum mi molestie tempor. Morbi vitae viverra odio. Pellentesque ac velit egestas, luctus arcu non, laoreet mauris. Sed in ipsum tempor, consequat odio in, porttitor ante. Ut mauris ligula, volutpat in sodales in, porta non odio. Pellentesque tempor urna vitae mi vestibulum, nec venenatis nulla lobortis. Proin at gravida ante. Mauris auctor purus at lacus maximus euismod. Pellentesque vulputate massa ut nisl hendrerit, eget elementum libero iaculis.
-						</p>
+                <div class="col-11 col-md-5 col-lg-4 m-lr-auto">
+                    <div class="how-bor1 ">
+                        <div class="hov-img0">
+                            <img src="images/about-01.jpg" alt="IMG">
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-						<div class="bor16 p-l-29 p-b-9 m-t-22">
-							<p class="stext-114 cl6 p-r-40 p-b-11">
-								Creativity is just connecting things. When you ask creative people how they did something, they feel a little guilty because they didn't really do it, they just saw something. It seemed obvious to them after a while.
-							</p>
+            <div class="row">
+                <div class="order-md-2 col-md-7 col-lg-8 p-b-30">
+                    <div class="p-t-7 p-l-85 p-l-15-lg p-l-0-md">
+                        <h3 class="mtext-111 cl2 p-b-16">
+                            Our Mission
+                        </h3>
 
-							<span class="stext-111 cl8">
-								- Steve Job’s 
-							</span>
-						</div>
-					</div>
-				</div>
+                        <p class="stext-113 cl6 p-b-26">
+                            Mauris non lacinia magna. Sed nec lobortis dolor. Vestibulum rhoncus dignissim risus, sed
+                            consectetur erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames
+                            ac turpis egestas. Nullam maximus mauris sit amet odio convallis, in pharetra magna gravida.
+                            Praesent sed nunc fermentum mi molestie tempor. Morbi vitae viverra odio. Pellentesque ac
+                            velit egestas, luctus arcu non, laoreet mauris. Sed in ipsum tempor, consequat odio in,
+                            porttitor ante. Ut mauris ligula, volutpat in sodales in, porta non odio. Pellentesque
+                            tempor urna vitae mi vestibulum, nec venenatis nulla lobortis. Proin at gravida ante. Mauris
+                            auctor purus at lacus maximus euismod. Pellentesque vulputate massa ut nisl hendrerit, eget
+                            elementum libero iaculis.
+                        </p>
 
-				<div class="order-md-1 col-11 col-md-5 col-lg-4 m-lr-auto p-b-30">
-					<div class="how-bor2">
-						<div class="hov-img0">
-							<img src="images/about-02.jpg" alt="IMG">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>	
+                        <div class="bor16 p-l-29 p-b-9 m-t-22">
+                            <p class="stext-114 cl6 p-r-40 p-b-11">
+                                Creativity is just connecting things. When you ask creative people how they did
+                                something, they feel a little guilty because they didn't really do it, they just saw
+                                something. It seemed obvious to them after a while.
+                            </p>
+
+                            <span class="stext-111 cl8">
+                                - Steve Job’s
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="order-md-1 col-11 col-md-5 col-lg-4 m-lr-auto p-b-30">
+                    <div class="how-bor2">
+                        <div class="hov-img0">
+                            <img src="images/about-02.jpg" alt="IMG">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </div>
