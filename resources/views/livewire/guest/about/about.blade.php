a0ede9ae3703f89d0e57714fb8500ab6a878304d
Judel Fintch <judfintch@gmail.com>
2024-11-21 16:08:09 +0200

    feat :adding about text

--- resources/views/livewire/guest/about/about.blade.php
+++ resources/views/livewire/guest/about/about.blade.php
@@ -19,7 +19,8 @@
                         </h3>
 
                         <p class="stext-113 cl6 p-b-26">
-                            C'est en observant la scène mode à Lubumbashi que <b>Moise A. NGONGO</b> a perçu une opportunité
+                            C'est en observant la scène mode à Lubumbashi que <b>Moise A. NGONGO</b> a perçu une
+                            opportunité
                             unique : créer une marque qui non seulement suit les tendances internationales, mais célèbre
                             aussi l’héritage local. Cette dualité entre global et local est au cœur de la philosophie
                             d’Urban Store. Dès ses débuts, Urban Store a misé sur la qualité, l’authenticité, et une
@@ -30,11 +31,12 @@
                         </p>
 
 
-						<h3 class="mtext-111 cl2 p-b-16"> Notre vision :</h3>
+                        <h3 class="mtext-111 cl2 p-b-16"> Notre vision :</h3>
                         <p class="stext-113 cl6 p-b-26">
                             Urban Store est né avec une mission claire : réinventer le style urbain et contemporain en
                             proposant des produits qui reflètent les besoins et les aspirations de ses clients. Fondée
-                            par <b>Moise A. NGONGO</b>, l’entreprise se positionne comme un pont entre les tendances mondiales
+                            par <b>Moise A. NGONGO</b>, l’entreprise se positionne comme un pont entre les tendances
+                            mondiales
                             et les influences locales, faisant de la mode un puissant vecteur d'expression personnelle
                             et culturelle.
 
@@ -59,26 +61,22 @@
                         </h3>
 
                         <p class="stext-113 cl6 p-b-26">
-                            Mauris non lacinia magna. Sed nec lobortis dolor. Vestibulum rhoncus dignissim risus, sed
-                            consectetur erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-                            ac turpis egestas. Nullam maximus mauris sit amet odio convallis, in pharetra magna gravida.
-                            Praesent sed nunc fermentum mi molestie tempor. Morbi vitae viverra odio. Pellentesque ac
-                            velit egestas, luctus arcu non, laoreet mauris. Sed in ipsum tempor, consequat odio in,
-                            porttitor ante. Ut mauris ligula, volutpat in sodales in, porta non odio. Pellentesque
-                            tempor urna vitae mi vestibulum, nec venenatis nulla lobortis. Proin at gravida ante. Mauris
-                            auctor purus at lacus maximus euismod. Pellentesque vulputate massa ut nisl hendrerit, eget
-                            elementum libero iaculis.
+							La mission de Urban Store est simple mais puissante :
+							“Offrir à chacun les moyens de s’exprimer à travers un style qui leur ressemble.”
+							L’entreprise vise à devenir une destination incontournable pour tous ceux qui recherchent des produits modernes, de qualité, et conçus pour inspirer. Urban Store s’engage également à intégrer des pratiques responsables, en collaborant avec des marques respectueuses de l’environnement et des valeurs humaines.
+							
                         </p>
 
                         <div class="bor16 p-l-29 p-b-9 m-t-22">
                             <p class="stext-114 cl6 p-r-40 p-b-11">
-                                Creativity is just connecting things. When you ask creative people how they did
-                                something, they feel a little guilty because they didn't really do it, they just saw
-                                something. It seemed obvious to them after a while.
+                                Nous croyons que la mode est plus qu’un simple vêtement. C’est un
+                                moyen de raconter son histoire, d’affirmer son identité et de célébrer la culture.
+                                Alors, "Find Your Style" et rejoignez-nous dans ce mouvement qui mélange modernité et
+                                authenticité.
                             </p>
 
                             <span class="stext-111 cl8">
-                                - Steve Job’s
+                                - Urban
                             </span>
                         </div>
                     </div>
