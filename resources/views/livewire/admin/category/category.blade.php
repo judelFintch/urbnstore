934d4690f07a1c7dd5d8142542cde3ed36badc80
Judel Fintch <judfintch@gmail.com>
2024-11-28 13:03:44 +0200

    feat :scalfolding category compt

--- resources/views/livewire/admin/category/category.blade.php
+++ resources/views/livewire/admin/category/category.blade.php
@@ -1,16 +1,24 @@
 <div>
-    @include('livewire.admin.partials.sidebar')
-    @include('livewire.admin.partials.header')
-    <div class="nk-content ">
-        <div class="container-fluid">
-            <div class="nk-content-inner">
-                <div class="nk-content-body">
-                    @include('livewire.admin.category.partials.header')
-                    @include('livewire.admin.category.partials.list')
-                    @include('livewire.admin.category.partials.create')
+    <!-- main @s -->
+    <div class="nk-main ">
+        <!-- wrap @s -->
+        <div class="nk-wrap ">
+            <!-- main header @s -->
+
+            @include('livewire.admin.partials.header')
+
+            <div class="nk-content ">
+                <div class="container-fluid">
+                    <div class="nk-content-inner">
+                        <div class="nk-content-body">
+                            @include('livewire.admin.category.partials.header')
+                            @include('livewire.admin.category.partials.list')
+                            @include('livewire.admin.category.partials.create')
+                        </div>
+                    </div>
                 </div>
             </div>
+            @include('livewire.admin.partials.footer')
         </div>
     </div>
-    @include('livewire.admin.partials.footer')
 </div>
