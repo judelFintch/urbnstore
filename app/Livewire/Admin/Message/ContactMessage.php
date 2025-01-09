c400ad6a0bf87541fd5144ee7b0655c7c3cfa701
Judel Fintch <judfintch@gmail.com>
2025-01-09 13:30:54 +0200

    feat: adding message view

--- app/Livewire/Admin/Message/ContactMessage.php
+++ app/Livewire/Admin/Message/ContactMessage.php
@@ -3,14 +3,22 @@
 namespace App\Livewire\Admin\Message;
 
 use Livewire\Component;
+use App\Models\ContactMessage as Message;
 use Livewire\Attributes\Layout;
 
 #[Layout('layouts.app')]
 
 class ContactMessage extends Component
 {
+    public $messages;
+
+    public function mount()
+    {
+        $this->messages = Message::all();
+    }
+
     public function render()
     {
-        return view('livewire.admin.message.contact-message');
+        return view('livewire.admin.message.contact-message', ['messages' => $this->messages]);
     }
 }
