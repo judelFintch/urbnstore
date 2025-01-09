f2dd82da4d39030dd5c20340b560cca57861b0d7
Judel Fintch <judfintch@gmail.com>
2025-01-09 15:40:09 +0200

    fix :error grille load 2

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -56,26 +56,6 @@
     <script src="{{ asset('js/main.js') }}" defer></script>
     <script src="{{ asset('js/personaliz.js') }}" defer></script>
 
-    <!-- Initialize Scripts -->
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            // Select2 Initialization
-            $(".js-select2").each(function() {
-                $(this).select2({
-                    minimumResultsForSearch: 20,
-                    dropdownParent: $(this).next('.dropDownSelect2')
-                });
-            });
-
-            // Parallax Initialization
-            $('.parallax100').parallax100();
-        });
-
-        document.addEventListener("DOMContentLoaded", () => {
-            displayCart(); // Affiche le contenu du panier après le chargement du DOM
-        });
-    </script>
-
 
     <script src="{{ asset('js/cart.js') }}" defer></script>
 </body>
