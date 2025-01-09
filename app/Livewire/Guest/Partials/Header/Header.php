ebd2c72bdac84af158b7a5278d77543993cc42b2
Judel Fintch <judfintch@gmail.com>
2025-01-09 13:48:47 +0200

    feat: limit category filter

--- app/Livewire/Guest/Partials/Header/Header.php
+++ app/Livewire/Guest/Partials/Header/Header.php
@@ -11,14 +11,13 @@ use App\Models\{
 
 class Header extends Component
 {
-
     public $categoryArticles;
     public $defaultCategoryArticles;
     public $defaultUrl;
     const IS_TYPE = true;
     public function mount($getDefaultCategoryArticles = null)
     {
-        $this->categoryArticles = CategoryArticles::select("id", "name", "slug")->where('is_active', self::IS_TYPE)->get();
+        $this->categoryArticles = CategoryArticles::select("id", "name", "slug")->where('is_active', self::IS_TYPE)->take(5)->get();
         $this->defaultCategoryArticles = $getDefaultCategoryArticles ?: Helpers::getDefaultCategory();
         $this->defaultUrl = Helpers::getDefaultUrlProduct();
     }
