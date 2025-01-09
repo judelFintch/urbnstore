0b0f5ed9190469a070e8c04db736b1b5890d2a5c
Judel Fintch <judfintch@gmail.com>
2025-01-09 15:09:14 +0200

    fix: header component codes

--- app/Livewire/Guest/Partials/Header/Header.php
+++ app/Livewire/Guest/Partials/Header/Header.php
@@ -1,29 +1,33 @@
 <?php
-
 namespace App\Livewire\Guest\Partials\Header;
 
 use Livewire\Component;
-use App\Helpers\Helpers;
-
-use App\Models\{
-    CategoryArticles,
-};
+use App\Models\CategoryArticles;
 
 class Header extends Component
 {
     public $categoryArticles;
     public $defaultCategoryArticles;
     public $defaultUrl;
+
+    // Définir directement les constantes ici
+    const DEFAULT_CATEGORY = 'all_categories';
+    const DEFAULT_URL_PRODUCT = 'all_products';
     const IS_TYPE = true;
-    public function mount($getDefaultCategoryArticles = null)
+
+    public function mount($providedDefaultCategory = null)
     {
-        $this->categoryArticles = CategoryArticles::select("id", "name", "slug")->where('is_active', self::IS_TYPE)->take(5)->get();
-        $this->defaultCategoryArticles = $getDefaultCategoryArticles ?: Helpers::getDefaultCategory();
-        $this->defaultUrl = Helpers::getDefaultUrlProduct();
+        $this->categoryArticles = CategoryArticles::select("id", "name", "slug")
+            ->where('is_active', self::IS_TYPE)
+            ->take(5)
+            ->get();
+
+        $this->defaultCategoryArticles = $providedDefaultCategory ?: self::DEFAULT_CATEGORY;
+        $this->defaultUrl = self::DEFAULT_URL_PRODUCT;
     }
+
     public function render()
     {
-
         return view('livewire.guest.partials.header.header');
     }
 }
