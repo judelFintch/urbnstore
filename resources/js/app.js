3068db67ebe2f6b85db1f8420dce18c17f6c6235
Judel Fintch <judfintch@gmail.com>
2025-01-15 15:04:42 +0200

    feat: remove compile asset

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,75 @@
-// Daterangepicker
+// Vendor JS
+import "../vendor/jquery/jquery-3.2.1.min.js"; // Toujours le premier
+import "../vendor/bootstrap/js/popper.js"; // Avant Bootstrap
+import "../vendor/bootstrap/js/bootstrap.min.js"; // Bootstrap JS
+import "../vendor/select2/select2.min.js"; // Select2 après jQuery
+import "../vendor/daterangepicker/moment.min.js"; // Moment.js autonome
+import "../vendor/daterangepicker/daterangepicker.js"; // Après Moment.js et jQuery
+import "../vendor/slick/slick.min.js"; // Slick Slider
+import "../vendor/animsition/js/animsition.min.js"; // Animsition
+import "../vendor/parallax100/parallax100.js"; // Parallax100
+import "../vendor/MagnificPopup/jquery.magnific-popup.min.js"; // MagnificPopup
+import "../vendor/isotope/isotope.pkgd.min.js"; // Isotope autonome
+import "../vendor/sweetalert/sweetalert.min.js"; // SweetAlert autonome
+import "../vendor/perfect-scrollbar/perfect-scrollbar.min.js"; // Perfect Scrollbar autonome
 
-import "../vendor/jquery/jquery-3.2.1.min.js";
-import "../vendor/animsition/js/animsition.min.js";
-import "../vendor/bootstrap/js/popper.js";
-import "../vendor/select2/select2.min.js";
-import "../vendor/daterangepicker/moment.min.js";
-import "../vendor/daterangepicker/daterangepicker.js";
-// Slick Slider
-import "../vendor/slick/slick.min.js";
-// Parallax100
-import "../vendor/parallax100/parallax100.js";
-// MagnificPopup
-import "../vendor/MagnificPopup/jquery.magnific-popup.min.js";
-// Isotope
-import "../vendor/isotope/isotope.pkgd.min.js";
-// SweetAlert
-import "../vendor/sweetalert/sweetalert.min.js";
-import "../vendor/perfect-scrollbar/perfect-scrollbar.min.js";
-// Perfect Scrollbar
-import "./slick-custom.js";
-import "./gallerylb.js";
-import "./addwish.js";
-import "./pscroll.js";
-import "./main.js";
+// Custom JS
+import "./main.js"; // Mettez `main.js` plus tôt si c'est un fichier central
+import "./slick-custom.js"; // Après Slick
+import "./gallerylb.js"; // Après MagnificPopup
+import "./addwish.js"; // Après SweetAlert
+import "./pscroll.js"; // Après Perfect Scrollbar
+import "./personaliz.js"; // Autres scripts personnalisés
+import "./cart.js"; // Doit être exécuté en dernier
 
-import "./personaliz.js";
-import "./cart.js";
+document.addEventListener("DOMContentLoaded", function () {
+    // Wishlist Initialization
+    $(".js-addwish-b2").each(function () {
+        var nameProduct = $(this).parent().parent().find(".js-name-b2").html();
+        $(this).on("click", function () {
+            swal(nameProduct, "is added to wishlist !", "success");
+            $(this).addClass("js-addedwish-b2");
+            $(this).off("click");
+        });
+    });
+
+    // Cart Initialization
+    $(".js-addcart-detail").each(function () {
+        var nameProduct = $(this)
+            .parent()
+            .parent()
+            .parent()
+            .parent()
+            .find(".js-name-detail")
+            .html();
+        $(this).on("click", function () {
+            swal(nameProduct, "is added to cart !", "success");
+        });
+    });
+
+    // Perfect Scrollbar Initialization
+    $(".js-pscroll").each(function () {
+        $(this).css("position", "relative");
+        $(this).css("overflow", "hidden");
+        var ps = new PerfectScrollbar(this, {
+            wheelSpeed: 1,
+            scrollingThreshold: 1000,
+            wheelPropagation: false,
+        });
+
+        $(window).on("resize", function () {
+            ps.update();
+        });
+    });
+
+    // Select2 Initialization
+    $(".js-select2").each(function () {
+        $(this).select2({
+            minimumResultsForSearch: 20,
+            dropdownParent: $(this).next(".dropDownSelect2"),
+        });
+    });
+
+    // Parallax100 Initialization
+    $(".parallax100").parallax100();
+});
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,8 +7,8 @@
     <title>{{ $title ?? 'Urban Store' }}</title>
     <link rel="icon" type="image/png" href="{{ asset('images/icons/favicon.png') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('build/assets/app.css') }}">
     
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="animsition">
@@ -23,60 +23,88 @@
         <x-backtop></x-backtop>
     @endif
 </body>
+
+
+<!-- JavaScript Dependencies -->
+<script src="{{ asset('vendor/jquery/jquery-3.2.1.min.js') }}" defer></script>
+<script src="{{ asset('vendor/animsition/js/animsition.min.js') }}" defer></script>
+<script src="{{ asset('vendor/bootstrap/js/popper.js') }}" defer></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}" defer></script>
+<script src="{{ asset('vendor/select2/select2.min.js') }}" defer></script>
+
+<script src="{{ asset('vendor/slick/slick.min.js') }}" defer></script>
+<script src="{{ asset('vendor/parallax100/parallax100.js') }}" defer></script>
+
+<script src="{{ asset('vendor/MagnificPopup/jquery.magnific-popup.min.js') }}" defer></script>
+<script src="{{ asset('vendor/isotope/isotope.pkgd.min.js') }}" defer></script>
+<script src="{{ asset('vendor/sweetalert/sweetalert.min.js') }}" defer></script>
+<script src="{{ asset('vendor/perfect-scrollbar/perfect-scrollbar.min.js') }}" defer></script>
+
+<!-- Custom Scripts -->
+<script src="{{ asset('js/slick-custom.js') }}" defer></script>
+<script src="{{ asset('js/gallerylb.js') }}" defer></script>
+<script src="{{ asset('js/addwish.js') }}" defer></script>
+<script src="{{ asset('js/pscroll.js') }}" defer></script>
+<script src="{{ asset('js/main.js') }}" defer></script>
+<script src="{{ asset('js/personaliz.js') }}" defer></script>
+
+
 <script>
-
-   
-    $('.js-addwish-b2').on('click', function (e) {
-        e.preventDefault();
-    });
-
-    $('.js-addwish-b2').each(function () {
-        var nameProduct = $(this).parent().parent().find('.js-name-b2').html();
-        $(this).on('click', function () {
-            swal(nameProduct, "is added to wishlist !", "success");
-
-            $(this).addClass('js-addedwish-b2');
-            $(this).off('click');
+    document.addEventListener("DOMContentLoaded", function () {
+        // Initialisez ici vos plugins et scripts personnalisés
+        $(".js-addwish-b2").on("click", function (e) {
+            e.preventDefault();
         });
-    });
 
-    $('.js-addwish-detail').each(function () {
-        var nameProduct = $(this).parent().parent().parent().find('.js-name-detail').html();
+        $(".js-addwish-b2").each(function () {
+            var nameProduct = $(this).parent().parent().find(".js-name-b2").html();
+            $(this).on("click", function () {
+                swal(nameProduct, "is added to wishlist !", "success");
 
-        $(this).on('click', function () {
-            swal(nameProduct, "is added to wishlist !", "success");
-
-            $(this).addClass('js-addedwish-detail');
-            $(this).off('click');
+                $(this).addClass("js-addedwish-b2");
+                $(this).off("click");
+            });
         });
-    });
 
-    /*---------------------------------------------*/
-
-    $('.js-addcart-detail').each(function () {
-        var nameProduct = $(this).parent().parent().parent().parent().find('.js-name-detail').html();
-        $(this).on('click', function () {
-            swal(nameProduct, "is added to cart !", "success");
+        $(".js-addcart-detail").each(function () {
+            var nameProduct = $(this)
+                .parent()
+                .parent()
+                .parent()
+                .parent()
+                .find(".js-name-detail")
+                .html();
+            $(this).on("click", function () {
+                swal(nameProduct, "is added to cart !", "success");
+            });
         });
+
+        $(".js-pscroll").each(function () {
+            $(this).css("position", "relative");
+            $(this).css("overflow", "hidden");
+            var ps = new PerfectScrollbar(this, {
+                wheelSpeed: 1,
+                scrollingThreshold: 1000,
+                wheelPropagation: false,
+            });
+
+            $(window).on("resize", function () {
+                ps.update();
+            });
+        });
+
+        // Initialisation Select2
+        $(".js-select2").each(function () {
+            $(this).select2({
+                minimumResultsForSearch: 20,
+                dropdownParent: $(this).next(".dropDownSelect2"),
+            });
+        });
+
+        // Initialisation Parallax100
+        $(".parallax100").parallax100();
     });
 
 </script>
-<!--===============================================================================================-->
-<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-<script>
-    $('.js-pscroll').each(function () {
-        $(this).css('position', 'relative');
-        $(this).css('overflow', 'hidden');
-        var ps = new PerfectScrollbar(this, {
-            wheelSpeed: 1,
-            scrollingThreshold: 1000,
-            wheelPropagation: false,
-        });
-
-        $(window).on('resize', function () {
-            ps.update();
-        })
-    });
-
 
 </html>
