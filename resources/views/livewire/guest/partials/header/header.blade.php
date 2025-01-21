58cb41537411ad76a90dfa300784b043dda5ff0e
Judel Fintch <judfintch@gmail.com>
2025-01-21 14:17:59 +0200

    feat: ui nav princapale

--- resources/views/livewire/guest/partials/header/header.blade.php
+++ resources/views/livewire/guest/partials/header/header.blade.php
@@ -5,12 +5,12 @@
         <div class="container-menu-desktop trans-03">
             <div class="wrap-menu-desktop">
                 <nav class="limiter-menu-desktop p-l-45">
-                    <!-- Logo -->
+                    <!-- Logo desktop -->
                     <a href="{{ route('home.index') }}" class="logo" aria-label="Accueil">
                         <img src="{{ asset('images/icons/lg.png') }}" alt="Logo" />
                     </a>
 
-                    <!-- Desktop Menu -->
+                    <!-- Menu desktop -->
                     <div class="menu-desktop">
                         <ul class="main-menu">
                             <li class="{{ request()->routeIs('home.index') ? 'active-menu' : '' }}">
@@ -38,96 +38,246 @@
                         </ul>
                     </div>
 
-                    <!-- Icons -->
+                    <!-- Icon header -->
                     <div class="wrap-icon-header flex-w flex-r-m h-full">
-                        <!-- Search Icon -->
-                        <div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 js-show-modal-search">
-                            <i class="fas fa-search" aria-hidden="true"></i>
-                        </div>
-                        <!-- Cart Icon -->
-                        <div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart" data-notify="2">
-                            <i class="fas fa-shopping-cart" aria-hidden="true"></i>
+                        <div class="flex-c-m h-full p-r-24">
+                            <div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 js-show-modal-search">
+                                <i class="zmdi zmdi-search"></i>
+                            </div>
                         </div>
 
-                        <!-- User Options -->
-                        @guest
-                            <a href="{{ route('login') }}" class="icon-header-item cl2 hov-cl1 trans-04">
-                                <i class="fas fa-sign-in-alt"></i>
-                            </a>
-                        @else
-                            <div class="dropdown">
-                                <a href="#" class="icon-header-item dropdown-toggle" data-bs-toggle="dropdown">
-                                    <i class="fas fa-user-circle"></i> {{ Auth::user()->name }}
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="{{ route('dashboard') }}">Tableau de bord</a></li>
-                                    <li>
-                                        <a class="dropdown-item" href="{{ route('logout') }}" 
-                                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                           Déconnexion
-                                        </a>
-                                    </li>
-                                </ul>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
+                        <div class="flex-c-m h-full p-l-18 p-r-25 bor5">
+                            <div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart" data-notify="2">
+                                <i class="fas fa-shopping-cart" aria-hidden="true"></i>
                             </div>
-                        @endguest
+                        </div>
+
+                        <div class="flex-c-m h-full p-lr-19">
+                            <div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 js-show-sidebar">
+                                <i class="zmdi zmdi-menu"></i>
+                            </div>
+                        </div>
                     </div>
                 </nav>
             </div>
         </div>
 
-        <!-- Mobile Menu -->
+        <!-- Header Mobile -->
         <div class="wrap-header-mobile">
-            <a href="{{ route('home.index') }}" class="logo-mobile">
-                <img src="{{ asset('images/icons/lg.png') }}" alt="Logo" />
-            </a>
-            <div class="hamburger hamburger--squeeze">
-                <span class="hamburger-box"><span class="hamburger-inner"></span></span>
+            <!-- Logo moblie -->
+            <div class="logo-mobile">
+                <a href="{{ route('home.index') }}" class="logo-mobile">
+                    <img src="{{ asset('images/icons/lg.png') }}" alt="Logo" />
+                </a>
+            </div>
+
+            <!-- Icon header -->
+            <div class="wrap-icon-header flex-w flex-r-m h-full m-r-15">
+                <div class="flex-c-m h-full p-r-10">
+                    <div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 js-show-modal-search">
+                        <i class="zmdi zmdi-search"></i>
+                    </div>
+                </div>
+
+                <div class="flex-c-m h-full p-lr-10 bor5">
+                    <div class="icon-header-item cl2 hov-cl1 trans-04 p-lr-11 icon-header-noti js-show-cart"
+                        data-notify="2">
+                        <i class="zmdi zmdi-shopping-cart"></i>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Button show menu -->
+            <div class="btn-show-menu-mobile hamburger hamburger--squeeze">
+                <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                </span>
             </div>
         </div>
+
+
+        <!-- Menu Mobile -->
         <div class="menu-mobile">
             <ul class="main-menu-m">
-                <li><a href="{{ route('home.index') }}">Accueil</a></li>
-                <li class="menu-item-has-children">
-                    <a href="#">Boutique</a>
+                <li>
+                    <a href="index.html">Home</a>
                     <ul class="sub-menu-m">
-                        <li id="shop-loading-mobile" style="display: none;">Chargement...</li>
-                        @foreach ($categoryArticles as $categoryArticle)
-                            <li>
-                                <a href="{{ route('home.shop', ['id' => $categoryArticle->id, 'slug' => $categoryArticle->slug]) }}">
-                                    {{ $categoryArticle->name }}
-                                </a>
-                            </li>
-                        @endforeach
+                        <li><a href="index.html">Homepage 1</a></li>
+                        <li><a href="home-02.html">Homepage 2</a></li>
+                        <li><a href="home-03.html">Homepage 3</a></li>
                     </ul>
+                    <span class="arrow-main-menu-m">
+                        <i class="fa fa-angle-right" aria-hidden="true"></i>
+                    </span>
                 </li>
-                <li><a href="{{ route('home.about') }}">À propos</a></li>
-                <li><a href="{{ route('home.contact') }}">Contact</a></li>
-                @guest
-                    <li><a href="{{ route('login') }}">Connexion</a></li>
-                @else
-                    <li class="menu-item-has-children">
-                        <a href="#"><i class="fas fa-user-circle"></i> Mon Compte</a>
-                        <ul class="sub-menu-m">
-                            <li><a href="{{ route('dashboard') }}">Tableau de bord</a></li>
-                            <li>
-                                <a href="{{ route('logout') }}" 
-                                   onclick="event.preventDefault(); document.getElementById('logout-form-mobile').submit();">
-                                   Déconnexion
-                                </a>
-                                <form id="logout-form-mobile" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
-                @endguest
+
+                <li>
+                    <a href="product.html">Shop</a>
+                </li>
+
+                <li>
+                    <a href="shoping-cart.html" class="label1 rs1" data-label1="hot">Features</a>
+                </li>
+
+                <li>
+                    <a href="blog.html">Blog</a>
+                </li>
+
+                <li>
+                    <a href="about.html">About</a>
+                </li>
+
+                <li>
+                    <a href="contact.html">Contact</a>
+                </li>
             </ul>
+        </div>
+
+        <!-- Modal Search -->
+        <div class="modal-search-header flex-c-m trans-04 js-hide-modal-search">
+            <div class="container-search-header">
+                <button class="flex-c-m btn-hide-modal-search trans-04 js-hide-modal-search">
+                    <img src="images/icons/icon-close2.png" alt="CLOSE">
+                </button>
+
+                <form class="wrap-search-header flex-w p-l-15">
+                    <button class="flex-c-m trans-04">
+                        <i class="zmdi zmdi-search"></i>
+                    </button>
+                    <input class="plh3" type="text" name="search" placeholder="Search...">
+                </form>
+            </div>
         </div>
     </header>
 
-    
+    <!-- Sidebar -->
+    <aside class="wrap-sidebar js-sidebar">
+        <div class="s-full js-hide-sidebar"></div>
+
+        <div class="sidebar flex-col-l p-t-22 p-b-25">
+            <div class="flex-r w-full p-b-30 p-r-27">
+                <div class="fs-35 lh-10 cl2 p-lr-5 pointer hov-cl1 trans-04 js-hide-sidebar">
+                    <i class="zmdi zmdi-close"></i>
+                </div>
+            </div>
+
+            <div class="sidebar-content flex-w w-full p-lr-65 js-pscroll">
+                <ul class="sidebar-link w-full">
+                    <li class="p-b-13">
+                        <a href="index.html" class="stext-102 cl2 hov-cl1 trans-04">
+                            Home
+                        </a>
+                    </li>
+
+                    <li class="p-b-13">
+                        <a href="#" class="stext-102 cl2 hov-cl1 trans-04">
+                            My Wishlist
+                        </a>
+                    </li>
+
+                    <li class="p-b-13">
+                        <a href="#" class="stext-102 cl2 hov-cl1 trans-04">
+                            My Account
+                        </a>
+                    </li>
+
+                    <li class="p-b-13">
+                        <a href="#" class="stext-102 cl2 hov-cl1 trans-04">
+                            Track Oder
+                        </a>
+                    </li>
+
+                    <li class="p-b-13">
+                        <a href="#" class="stext-102 cl2 hov-cl1 trans-04">
+                            Refunds
+                        </a>
+                    </li>
+
+                    <li class="p-b-13">
+                        <a href="#" class="stext-102 cl2 hov-cl1 trans-04">
+                            Help & FAQs
+                        </a>
+                    </li>
+                </ul>
+
+                <div class="sidebar-gallery w-full p-tb-30">
+                    <span class="mtext-101 cl5">
+                        @ CozaStore
+                    </span>
+
+                    <div class="flex-w flex-sb p-t-36 gallery-lb">
+                        <!-- item gallery sidebar -->
+                        <div class="wrap-item-gallery m-b-10">
+                            <a class="item-gallery bg-img1" href="images/gallery-01.jpg" data-lightbox="gallery"
+                                style="background-image: url('images/gallery-01.jpg');"></a>
+                        </div>
+
+                        <!-- item gallery sidebar -->
+                        <div class="wrap-item-gallery m-b-10">
+                            <a class="item-gallery bg-img1" href="images/gallery-02.jpg" data-lightbox="gallery"
+                                style="background-image: url('images/gallery-02.jpg');"></a>
+                        </div>
+
+                        <!-- item gallery sidebar -->
+                        <div class="wrap-item-gallery m-b-10">
+                            <a class="item-gallery bg-img1" href="images/gallery-03.jpg" data-lightbox="gallery"
+                                style="background-image: url('images/gallery-03.jpg');"></a>
+                        </div>
+
+                        <!-- item gallery sidebar -->
+                        <div class="wrap-item-gallery m-b-10">
+                            <a class="item-gallery bg-img1" href="images/gallery-04.jpg" data-lightbox="gallery"
+                                style="background-image: url('images/gallery-04.jpg');"></a>
+                        </div>
+
+                        <!-- item gallery sidebar -->
+                        <div class="wrap-item-gallery m-b-10">
+                            <a class="item-gallery bg-img1" href="images/gallery-05.jpg" data-lightbox="gallery"
+                                style="background-image: url('images/gallery-05.jpg');"></a>
+                        </div>
+
+                        <!-- item gallery sidebar -->
+                        <div class="wrap-item-gallery m-b-10">
+                            <a class="item-gallery bg-img1" href="images/gallery-06.jpg" data-lightbox="gallery"
+                                style="background-image: url('images/gallery-06.jpg');"></a>
+                        </div>
+
+                        <!-- item gallery sidebar -->
+                        <div class="wrap-item-gallery m-b-10">
+                            <a class="item-gallery bg-img1" href="images/gallery-07.jpg" data-lightbox="gallery"
+                                style="background-image: url('images/gallery-07.jpg');"></a>
+                        </div>
+
+                        <!-- item gallery sidebar -->
+                        <div class="wrap-item-gallery m-b-10">
+                            <a class="item-gallery bg-img1" href="images/gallery-08.jpg" data-lightbox="gallery"
+                                style="background-image: url('images/gallery-08.jpg');"></a>
+                        </div>
+
+                        <!-- item gallery sidebar -->
+                        <div class="wrap-item-gallery m-b-10">
+                            <a class="item-gallery bg-img1" href="images/gallery-09.jpg" data-lightbox="gallery"
+                                style="background-image: url('images/gallery-09.jpg');"></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="sidebar-gallery w-full">
+                    <span class="mtext-101 cl5">
+                        About Us
+                    </span>
+
+                    <p class="stext-108 cl6 p-t-27">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur maximus vulputate hendrerit.
+                        Praesent faucibus erat vitae rutrum gravida. Vestibulum tempus mi enim, in molestie sem
+                        fermentum
+                        quis.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </aside>
+
+
 
 </div>
