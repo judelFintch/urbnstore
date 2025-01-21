8f4a030a827e642c8022359f319b31cb9cd7abd2
Judel Fintch <judfintch@gmail.com>
2025-01-21 14:19:50 +0200

    feat: ui nav princapale mobile

--- resources/views/livewire/guest/partials/header/header.blade.php
+++ resources/views/livewire/guest/partials/header/header.blade.php
@@ -99,37 +99,22 @@
         <!-- Menu Mobile -->
         <div class="menu-mobile">
             <ul class="main-menu-m">
-                <li>
-                    <a href="index.html">Home</a>
-                    <ul class="sub-menu-m">
-                        <li><a href="index.html">Homepage 1</a></li>
-                        <li><a href="home-02.html">Homepage 2</a></li>
-                        <li><a href="home-03.html">Homepage 3</a></li>
-                    </ul>
-                    <span class="arrow-main-menu-m">
-                        <i class="fa fa-angle-right" aria-hidden="true"></i>
-                    </span>
-                </li>
-
-                <li>
-                    <a href="product.html">Shop</a>
-                </li>
-
-                <li>
-                    <a href="shoping-cart.html" class="label1 rs1" data-label1="hot">Features</a>
-                </li>
-
-                <li>
-                    <a href="blog.html">Blog</a>
-                </li>
-
-                <li>
-                    <a href="about.html">About</a>
-                </li>
-
-                <li>
-                    <a href="contact.html">Contact</a>
-                </li>
+            <li><a href="{{ route('home.index') }}">Accueil</a></li>
+            <li class="menu-item-has-children">
+                <a href="#">Boutique</a>
+                <ul class="sub-menu-m">
+                    <li id="shop-loading-mobile" style="display: none;">Chargement...</li>
+                    @foreach ($categoryArticles as $categoryArticle)
+                        <li>
+                            <a href="{{ route('home.shop', ['id' => $categoryArticle->id, 'slug' => $categoryArticle->slug]) }}">
+                                {{ $categoryArticle->name }}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </li>
+            <li><a href="{{ route('home.about') }}">À propos</a></li>
+            <li><a href="{{ route('home.contact') }}">Contact</a></li>
             </ul>
         </div>
 
