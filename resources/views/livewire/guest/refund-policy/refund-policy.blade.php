7259c4f476206961fa6eff559e4b21a5f9c8c5de
Judel Fintch <judfintch@gmail.com>
2025-01-21 10:45:54 +0200

    feat: page politique et reclamation

--- resources/views/livewire/guest/refund-policy/refund-policy.blade.php
+++ resources/views/livewire/guest/refund-policy/refund-policy.blade.php
@@ -30,43 +30,7 @@
                         Veuillez noter que les réclamations tardives ou incomplètes pourraient ne pas être prises en
                         charge.
                     </p>
-                </div>
-            </div>
 
-            <!-- Procédure de remboursement -->
-            <div class="row p-t-50">
-                <div class="col-md-6 order-md-2">
-                    <img src="images/refund-1.webp" alt="Procédure de remboursement" class="img-fluid rounded">
-                </div>
-                <div class="col-md-6 order-md-1">
-                    <h3 class="mtext-111 cl2 p-b-16">Procédure de Remboursement</h3>
-                    <p class="stext-113 cl6 p-b-26" style="text-align:justify;">
-                        Nous offrons des remboursements pour les produits répondant aux critères suivants :
-                    </p>
-                    <ul class="stext-113 cl6 p-l-30 p-b-26" style="list-style-type: disc; text-align:justify;">
-                        <li><strong>Produit défectueux :</strong> Présente des défauts de fabrication ou des dommages
-                            visibles.</li>
-                        <li><strong>Produit non conforme :</strong> Diffère de la description ou des spécifications
-                            annoncées.</li>
-                    </ul>
-                    <p class="stext-113 cl6 p-b-26" style="text-align:justify;">
-                        Une fois la réclamation validée, le remboursement sera traité dans un délai de <strong>7 jours
-                            ouvrables</strong>. Le montant remboursé sera crédité sur le mode de paiement utilisé lors
-                        de l'achat.
-                    </p>
-                    <p class="stext-113 cl6 p-b-26" style="text-align:justify;">
-                        Les frais de retour, le cas échéant, sont généralement à la charge du client, sauf si le produit
-                        reçu est défectueux ou incorrect.
-                    </p>
-                </div>
-            </div>
-
-            <!-- Assistance -->
-            <div class="row p-t-50">
-                <div class="col-md-6">
-                    <img src="images/refund-3.webp" alt="Assistance client" class="img-fluid rounded">
-                </div>
-                <div class="col-md-6">
                     <h3 class="mtext-111 cl2 p-b-16">Besoin d'Aide ?</h3>
                     <p class="stext-113 cl6 p-b-26" style="text-align:justify;">
                         Pour toute question ou assistance concernant votre réclamation ou remboursement, n'hésitez pas à
@@ -77,7 +41,8 @@
                             du Congo.</li>
                         <li><strong>Téléphones :</strong> (+243) 99 01 000 05, (+243) 99 99 907 56</li>
                         <li><strong>Site web :</strong> <a href="https://urbanbrands.store/"
-                                target="_blank">https://urbanbrands.store/</a></li>
+                                target="_blank">https://urbanbrands.store/</a>
+                        </li>
                     </ul>
                     <p class="stext-113 cl6 p-b-26" style="text-align:justify;">
                         Nous sommes déterminés à vous offrir la meilleure expérience possible et à résoudre vos
@@ -85,6 +50,8 @@
                     </p>
                 </div>
             </div>
+
+
         </div>
     </section>
 </div>
