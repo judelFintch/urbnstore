4ba599485a96ffabad9195b2dfa2812aca785866
Judel Fintch <judfintch@gmail.com>
2025-01-23 15:00:46 +0200

    fix: url link categories

--- resources/views/livewire/products/partials/sec-banner.blade.php
+++ resources/views/livewire/products/partials/sec-banner.blade.php
@@ -7,7 +7,8 @@
                 <div class="block1 wrap-pic-w">
                     <img src="{{Storage::url($banner->photo) ?? 'default_image.jpg' }}" alt="IMG-BANNER">
 
-                    <a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
+                    <a href="{{ route('home.shop', ['id' => $banner->id, 'slug' => $banner->slug]) }}"
+                        class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
                         <div class="block1-txt-child1 flex-col-l">
                             <span class="block1-name ltext-102 trans-04 p-b-8">
                                 {{ $banner->name }}
