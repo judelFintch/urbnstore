20d67d1936c666020e43b345c009e8dd0aff002f
judel fintch <judel@gmail.com>
2025-03-06 09:35:31 +0200

    feat: product dynamique slider done

--- resources/views/livewire/products/partials/product-grid.blade.php
+++ resources/views/livewire/products/partials/product-grid.blade.php
@@ -11,184 +11,81 @@
             <div class="tab01">
                 <!-- Nav tabs -->
                 <ul class="nav nav-tabs" role="tablist">
-                    <li class="nav-item p-b-10">
-                        <a class="nav-link active" data-toggle="tab" href="#best-seller" role="tab">Best Seller</a>
-                    </li>
-                    <li class="nav-item p-b-10">
-                        <a class="nav-link" data-toggle="tab" href="#featured" role="tab">Featured</a>
-                    </li>
-                    <li class="nav-item p-b-10">
-                        <a class="nav-link" data-toggle="tab" href="#sale" role="tab">Sale</a>
-                    </li>
-                    <li class="nav-item p-b-10">
-                        <a class="nav-link" data-toggle="tab" href="#top-rate" role="tab">Top Rate</a>
-                    </li>
+                    @php
+                        $categories = [
+                            'best-seller' => 'Best Seller',
+                            'featured' => 'Featured',
+                            'sale' => 'Sale',
+                            'top-rate' => 'Top Rate',
+                        ];
+                        $firstCategory = array_key_first($categories);
+                    @endphp
+
+                    @foreach($categories as $key => $category)
+                        <li class="nav-item p-b-10">
+                            <a class="nav-link {{ $loop->first ? 'active' : '' }}" 
+                               data-toggle="tab" href="#{{ $key }}" role="tab">
+                                {{ $category }}
+                            </a>
+                        </li>
+                    @endforeach
                 </ul>
 
                 <!-- Tab panes -->
                 <div class="tab-content p-t-50">
-                    <!-- Best Seller -->
-                    <div class="tab-pane fade show active" id="best-seller" role="tabpanel">
-                        <!-- Slide2 -->
-                        <div class="wrap-slick2">
-                            <div class="slick2">
-                                @foreach($products as $product)
-                                @php
+                    @foreach($categories as $key => $category)
+                        <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" 
+                             id="{{ $key }}" role="tabpanel">
+                            <div class="wrap-slick2">
+                                <div class="slick2">
+                                    @foreach($products as $product)
+                                        @php
+                                            $productUrl = route('show-product', [
+                                                'id' => $product->id,
+                                                'category' => optional($product->category)->name ?? 'uncategorized',
+                                                'slug' => $product->slug,
+                                            ]);
+                                        @endphp
 
-                                $productUrl = route('show-product', [
-                                    'id' => $product->id,
-                                    'category' => $product->category->name,
-                                    'slug' => $product->slug,
-                                ]);
-
-                                $categoryName = $product->category->name;
-                            @endphp
-                                    <div class="item-slick2 p-l-15 p-r-15 p-t-15 p-b-15">
-                                        <!-- Block2 -->
-                                        <div class="block2">
-                                            <div class="block2-pic hov-img0">
-                                                <img src="{{$product->getFirstImageUrl()}}" alt="IMG-PRODUCT">
-                                                <a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-                                                    Quick View
-                                                </a>
-                                            </div>
-                                            <div class="block2-txt flex-w flex-t p-t-14">
-                                                <div class="block2-txt-child1 flex-col-l">
-                                                    <a href="" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                                                        {{ $product->title }}
+                                        <div class="item-slick2 p-15">
+                                            <div class="block2">
+                                                <div class="block2-pic hov-img0">
+                                                    <img src="{{ $product->getFirstImageUrl() }}" 
+                                                         alt="IMG-PRODUCT" loading="lazy">
+                                                    <a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 
+                                                        p-lr-15 trans-04 js-show-modal1">
+                                                        Quick View
                                                     </a>
-                                                    <span class="stext-105 cl3">
-                                                        ${{ $product->price }}
-                                                    </span>
                                                 </div>
-                                                <div class="block2-txt-child2 flex-r p-t-3">
-                                                    <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
-                                                        <img class="icon-heart1 dis-block trans-04" src="{{ asset('images/icons/icon-heart-01.png') }}" alt="ICON">
-                                                        <img class="icon-heart2 dis-block trans-04 ab-t-l" src="{{ asset('images/icons/icon-heart-02.png') }}" alt="ICON">
-                                                    </a>
+
+                                                <div class="block2-txt flex-w flex-t p-t-14">
+                                                    <div class="block2-txt-child1 flex-col-l">
+                                                        <a href="{{ $productUrl }}" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+                                                            {{ $product->title ?? $product->name }}
+                                                        </a>
+                                                        <span class="stext-105 cl3">
+                                                            ${{ $product->price }}
+                                                        </span>
+                                                    </div>
+
+                                                    <div class="block2-txt-child2 flex-r p-t-3">
+                                                        <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+                                                            <img class="icon-heart1 dis-block trans-04" 
+                                                                 src="{{ asset('images/icons/icon-heart-01.png') }}" 
+                                                                 alt="ICON">
+                                                            <img class="icon-heart2 dis-block trans-04 ab-t-l" 
+                                                                 src="{{ asset('images/icons/icon-heart-02.png') }}" 
+                                                                 alt="ICON">
+                                                        </a>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                @endforeach
+                                    @endforeach
+                                </div>
                             </div>
                         </div>
-                    </div>
-
-                    <!-- Featured -->
-                    <div class="tab-pane fade" id="featured" role="tabpanel">
-                        <!-- Slide2 -->
-                        <div class="wrap-slick2">
-                            <div class="slick2">
-                                @foreach($products as $product)
-                                    <div class="item-slick2 p-l-15 p-r-15 p-t-15 p-b-15">
-                                        <!-- Block2 -->
-                                        <div class="block2">
-                                            <div class="block2-pic hov-img0">
-                                                <img src="{{$product->getFirstImageUrl()}}" alt="IMG-PRODUCT">
-                                                <a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-                                                    Quick View
-                                                </a>
-                                            </div>
-                                            <div class="block2-txt flex-w flex-t p-t-14">
-                                                <div class="block2-txt-child1 flex-col-l">
-                                                    <a href="" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                                                        {{ $product->title }}
-                                                    </a>
-                                                    <span class="stext-105 cl3">
-                                                        ${{ $product->price }}
-                                                    </span>
-                                                </div>
-                                                <div class="block2-txt-child2 flex-r p-t-3">
-                                                    <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
-                                                        <img class="icon-heart1 dis-block trans-04" src="{{ asset('images/icons/icon-heart-01.png') }}" alt="ICON">
-                                                        <img class="icon-heart2 dis-block trans-04 ab-t-l" src="{{ asset('images/icons/icon-heart-02.png') }}" alt="ICON">
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Sale -->
-                    <div class="tab-pane fade" id="sale" role="tabpanel">
-                        <!-- Slide2 -->
-                        <div class="wrap-slick2">
-                            <div class="slick2">
-                                @foreach($products as $product)
-                                    <div class="item-slick2 p-l-15 p-r-15 p-t-15 p-b-15">
-                                        <!-- Block2 -->
-                                        <div class="block2">
-                                            <div class="block2-pic hov-img0">
-                                                <img src="{{$product->getFirstImageUrl()}}" alt="IMG-PRODUCT">
-                                                <a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-                                                    Quick View
-                                                </a>
-                                            </div>
-                                            <div class="block2-txt flex-w flex-t p-t-14">
-                                                <div class="block2-txt-child1 flex-col-l">
-                                                    <a href="" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                                                        {{ $product->name }}
-                                                    </a>
-                                                    <span class="stext-105 cl3">
-                                                        ${{ $product->price }}
-                                                    </span>
-                                                </div>
-                                                <div class="block2-txt-child2 flex-r p-t-3">
-                                                    <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
-                                                        <img class="icon-heart1 dis-block trans-04" src="{{ asset('images/icons/icon-heart-01.png') }}" alt="ICON">
-                                                        <img class="icon-heart2 dis-block trans-04 ab-t-l" src="{{ asset('images/icons/icon-heart-02.png') }}" alt="ICON">
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Top Rate -->
-                    <div class="tab-pane fade" id="top-rate" role="tabpanel">
-                        <!-- Slide2 -->
-                        <div class="wrap-slick2">
-                            <div class="slick2">
-                                @foreach($products as $product)
-                                    <div class="item-slick2 p-l-15 p-r-15 p-t-15 p-b-15">
-                                        <!-- Block2 -->
-                                        <div class="block2">
-                                            <div class="block2-pic hov-img0">
-                                                <img src="{{$product->getFirstImageUrl()}}" alt="IMG-PRODUCT">
-                                                <a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
-                                                    Quick View
-                                                </a>
-                                            </div>
-                                            <div class="block2-txt flex-w flex-t p-t-14">
-                                                <div class="block2-txt-child1 flex-col-l">
-                                                    <a href="" class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                                                        {{ $product->name }}
-                                                    </a>
-                                                    <span class="stext-105 cl3">
-                                                        ${{ $product->price }}
-                                                    </span>
-                                                </div>
-                                                <div class="block2-txt-child2 flex-r p-t-3">
-                                                    <a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
-                                                        <img class="icon-heart1 dis-block trans-04" src="{{ asset('images/icons/icon-heart-01.png') }}" alt="ICON">
-                                                        <img class="icon-heart2 dis-block trans-04 ab-t-l" src="{{ asset('images/icons/icon-heart-02.png') }}" alt="ICON">
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-                    </div>
-
+                    @endforeach
                 </div>
             </div>
         </div>
