d23b552c70e84ab55fad488bc6cc6dc2fc8ab72d
judel fintch <judel@gmail.com>
2025-03-15 10:54:42 +0200

    fix:postion text

--- resources/views/livewire/products/partials/slide-product.blade.php
+++ resources/views/livewire/products/partials/slide-product.blade.php
@@ -109,113 +109,107 @@
         }
     </style>
     <section class="sec-product bg0 p-t-100 p-b-50">
+        <h3 class="ltext-105 cl5 txt-center respon1">
+            Nouvelle collection
+        </h3>
+        <!-- Tab01 -->
+        <div class="tab01">
+            <!-- Tab panes -->
+            <div class="tab-content p-t-50">
+                <div class="tab-pane fade show active">
+                    <div class="wrap-slick2" wire:ignore>
+                        <div class="slick2">
+                            @foreach ($filteredProducts as $product)
+                                @php
+                                    $productUrl = route('show-product', [
+                                        'id' => $product->id,
+                                        'category' => optional($product->category)->name ?? 'uncategorized',
+                                        'slug' => $product->slug,
+                                    ]);
+                                @endphp
 
-        <div class="container">
-            <div class="p-b-32">
-                <h3 class="ltext-105 cl5 txt-center respon1">
-                    Nouvelle collection
-                </h3>
-            </div>
+                                <div class="item-slick2 p-l-15 p-r-15 p-t-15 p-b-15">
+                                    <a href="{{ $productUrl }}" class="product-link">
+                                        <div class="block2">
+                                            <div class="block2-pic hov-img0">
+                                                <img src="{{ $product->getFirstImageUrl() }}" alt="IMG-PRODUCT"
+                                                    loading="lazy">
 
-            <!-- Tab01 -->
-            <div class="tab01">
+                                            </div>
 
-                <!-- Tab panes -->
-                <div class="tab-content p-t-50">
-                    <div class="tab-pane fade show active">
-                        <div class="wrap-slick2" wire:ignore>
-                            <div class="slick2">
-                                @foreach ($filteredProducts as $product)
-                                    @php
-                                        $productUrl = route('show-product', [
-                                            'id' => $product->id,
-                                            'category' => optional($product->category)->name ?? 'uncategorized',
-                                            'slug' => $product->slug,
-                                        ]);
-                                    @endphp
-
-                                    <div class="item-slick2 p-l-15 p-r-15 p-t-15 p-b-15">
-                                        <a href="{{ $productUrl }}" class="product-link">
-                                            <div class="block2">
-                                                <div class="block2-pic hov-img0">
-                                                    <img src="{{ $product->getFirstImageUrl() }}" alt="IMG-PRODUCT"
-                                                        loading="lazy">
-
+                                            <div class="block2-txt flex-w flex-t p-t-14">
+                                                <div class="block2-txt-child1 flex-col-l">
+                                                    <a href="{{ $productUrl }}"
+                                                        class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+                                                        {{ $product->title ?? $product->name }}
+                                                    </a>
+                                                    <span class="stext-105 cl3">
+                                                        ${{ $product->price }}
+                                                    </span>
                                                 </div>
 
-                                                <div class="block2-txt flex-w flex-t p-t-14">
-                                                    <div class="block2-txt-child1 flex-col-l">
-                                                        <a href="{{ $productUrl }}"
-                                                            class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-                                                            {{ $product->title ?? $product->name }}
-                                                        </a>
-                                                        <span class="stext-105 cl3">
-                                                            ${{ $product->price }}
-                                                        </span>
-                                                    </div>
-
-                                                    <div class="block2-txt-child2 flex-r p-t-3">
-                                                        <a href="#"
-                                                            class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
-                                                            <img class="icon-heart1 dis-block trans-04"
-                                                                src="{{ asset('images/icons/icon-heart-01.png') }}"
-                                                                alt="ICON">
-                                                            <img class="icon-heart2 dis-block trans-04 ab-t-l"
-                                                                src="images/icons/icon-heart-02.png" alt="ICON">
-                                                        </a>
-                                                    </div>
+                                                <div class="block2-txt-child2 flex-r p-t-3">
+                                                    <a href="#"
+                                                        class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+                                                        <img class="icon-heart1 dis-block trans-04"
+                                                            src="{{ asset('images/icons/icon-heart-01.png') }}"
+                                                            alt="ICON">
+                                                        <img class="icon-heart2 dis-block trans-04 ab-t-l"
+                                                            src="images/icons/icon-heart-02.png" alt="ICON">
+                                                    </a>
                                                 </div>
                                             </div>
-                                        </a>
-                                    </div>
-                                @endforeach
-                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            @endforeach
                         </div>
                     </div>
+                </div>
 
 
-                    <script>
-                        function initSlickSlider() {
-                            const slider = $('.slick2');
+                <script>
+                    function initSlickSlider() {
+                        const slider = $('.slick2');
 
-                            if (slider.hasClass('slick-initialized')) {
-                                slider.slick('unslick');
-                            }
-
-                            slider.slick({
-                                slidesToShow: 4,
-                                slidesToScroll: 1,
-                                infinite: true,
-                                dots: false,
-                                autoplay: true,
-                                autoplaySpeed: 2500,
-                                prevArrow: '<button type="button" class="slick-prev"></button>',
-                                nextArrow: '<button type="button" class="slick-next"></button>',
-                                responsive: [{
-                                        breakpoint: 1024,
-                                        settings: {
-                                            slidesToShow: 3
-                                        }
-                                    },
-                                    {
-                                        breakpoint: 768,
-                                        settings: {
-                                            slidesToShow: 2
-                                        }
-                                    },
-                                    {
-                                        breakpoint: 576,
-                                        settings: {
-                                            slidesToShow: 1
-                                        }
-                                    }
-                                ]
-                            });
+                        if (slider.hasClass('slick-initialized')) {
+                            slider.slick('unslick');
                         }
 
-                        document.addEventListener('livewire:update', initSlickSlider);
-                        document.addEventListener('DOMContentLoaded', initSlickSlider);
-                    </script>
+                        slider.slick({
+                            slidesToShow: 4,
+                            slidesToScroll: 1,
+                            infinite: true,
+                            dots: false,
+                            autoplay: true,
+                            autoplaySpeed: 2500,
+                            prevArrow: '<button type="button" class="slick-prev"></button>',
+                            nextArrow: '<button type="button" class="slick-next"></button>',
+                            responsive: [{
+                                    breakpoint: 1024,
+                                    settings: {
+                                        slidesToShow: 3
+                                    }
+                                },
+                                {
+                                    breakpoint: 768,
+                                    settings: {
+                                        slidesToShow: 2
+                                    }
+                                },
+                                {
+                                    breakpoint: 576,
+                                    settings: {
+                                        slidesToShow: 1
+                                    }
+                                }
+                            ]
+                        });
+                    }
+
+                    document.addEventListener('livewire:update', initSlickSlider);
+                    document.addEventListener('DOMContentLoaded', initSlickSlider);
+                </script>
 
 
     </section>
