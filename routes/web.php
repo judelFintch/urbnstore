e460c0acae42b2b39761daad9a2ed0d6ac9870be
judel fintch <judel@gmail.com>
2025-03-22 10:23:19 +0200

    feat: change url name success

--- routes/web.php
+++ routes/web.php
@@ -79,7 +79,7 @@ Route::prefix('/')->group(function () {
 Route::group(['prefix' => 'process'], function () {
     //Route::post('/payment', [FlexPayController::class, 'handlePayment'])->name('payment');
     Route::post('/payment', [CheckoutOrder::class, 'handlePayment'])->name('payment');
-    Route::get('/payment/success', Success::class)->name('accepted.payment');
+    Route::get('/accepted/payment', \App\Livewire\Payment\Success::class)->name('accepted.payment');
     Route::get('/rejected/payment', \App\Livewire\Payment\Reject::class)->name('rejected.payment');
     Route::get('/maxi-notify/payment', [MaxiNotifyPaymentController::class, 'handlePayment'])->name('maxi-notify.payment');
 });
