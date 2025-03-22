85b1bc9e720cfaffab5600a67d16f4f52a43ae5e
judel fintch <judel@gmail.com>
2025-03-22 10:16:05 +0200

    feat: reject page paiement

--- app/Livewire/Payment/Reject.php
+++ app/Livewire/Payment/Reject.php
@@ -3,9 +3,11 @@
 namespace App\Livewire\Payment;
 
 use Livewire\Component;
+use Livewire\Attributes\Layout;
 
 class Reject extends Component
 {
+    #[Layout('layouts.guest')]
     public function render()
     {
         return view('livewire.payment.reject');
