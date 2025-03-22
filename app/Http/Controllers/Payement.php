2e1686dd825ccfd614a318b0880d57cedff73042
judel fintch <judel@gmail.com>
2025-03-22 11:34:04 +0200

    feat: addin request

--- app/Http/Controllers/Payement.php
+++ app/Http/Controllers/Payement.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Log;
+use App\Models\Product;
+use App\Models\Order;
+use App\Models\DetailsOrder;
+use Devscast\Maxicash\Client as Maxicash;
+use Devscast\Maxicash\Credential;
+use Devscast\Maxicash\Environment;
+use Devscast\Maxicash\PaymentEntry;
 
 class Payement extends Controller
 {
     //
 
-    echo "Hello World";
+    public function handlePayment(Request $request)
+    {
+      dd($request);
+    }
 }
