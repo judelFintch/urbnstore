a4e6b88528de369d1526dc9ee70337cfa37ffef0
judel fintch <judel@gmail.com>
2025-04-02 10:37:56 +0200

    feat: show users list done

--- resources/views/livewire/admin/user/user-dashboard.blade.php
+++ resources/views/livewire/admin/user/user-dashboard.blade.php
@@ -9,7 +9,7 @@
                 <div class="container-fluid">
                     <div class="nk-content-inner">
                         <div class="nk-content-body">
-                            @include('livewire.admin.category.partials.header')
+                            @include('livewire.admin.user.partials.header')
                             @livewire('admin.user.partials.list-user')
                         </div>
                     </div>
