cfcb351c68e5ca7e7c422718f1e585c7d1264dc6
Judel Fintch <71493804+judelFintch@users.noreply.github.com>
2025-06-11 21:58:02 +0200

    Group JS assets with Vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,21 @@
-import "bootstrap";
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+
+import 'bootstrap';
+
+import '../vendor/animsition/js/animsition.min.js';
+import '../vendor/select2/select2.min.js';
+import '../vendor/daterangepicker/moment.min.js';
+import '../vendor/daterangepicker/daterangepicker.js';
+import '../vendor/slick/slick.min.js';
+import './slick-custom.js';
+import '../vendor/parallax100/parallax100.js';
+import '../vendor/MagnificPopup/jquery.magnific-popup.min.js';
+import '../vendor/isotope/isotope.pkgd.min.js';
+import '../vendor/sweetalert/sweetalert.min.js';
+import '../vendor/perfect-scrollbar/perfect-scrollbar.min.js';
+import './gallerylb.js';
+import './addwish.js';
+import './pscroll.js';
+import './main.js';
 
